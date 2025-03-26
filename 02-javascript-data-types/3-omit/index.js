b2784b0d4d0c9503df775b5cadc2151d87b19b6f
Andrey <sergeev_a@joy.money>
2025-03-26 23:16:05 +0700

    Добавил функцию для исключения свойств объекта с помощью Object.entries

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,7 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
-
+  const fieldsCollection = new Set(fields);
+  const filteredObject = Object.entries(obj).filter(([key]) => !fieldsCollection.has(key));
+  return {...Object.fromEntries(filteredObject)};
 };
