44a59d762abb21dfc9a1f8fa8024a7bf9b3bef61
Andrey <sergeev_a@joy.money>
2025-03-26 23:04:52 +0700

    Добавил функцию для выбора свойств переданного объекта с помощью Object.entries

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,7 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  const fieldsCollection = new Set(fields);
+  const filteredObject = Object.entries(obj).filter(([key]) => fieldsCollection.has(key));
+  return {...Object.fromEntries(filteredObject)};
 };
