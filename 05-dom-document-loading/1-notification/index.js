10c35dc32e876c0d4ff0e36ab6ced03aa7893080
Andrey <sergeev_a@joy.money>
2025-04-07 02:27:18 +0700

    Реализовал открытие notification и автоматическое его скрытие

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,65 @@
 export default class NotificationMessage {
+  element;
+  static lastShownNotification;
 
+  constructor(text = '', {duration = 0, type = ''} = {}) {
+    this.text = text;
+    this.duration = duration;
+    this.type = type;
+    this.element = this.createElement();
+  }
+
+  // Templates
+  createNotificationTemplate() {
+    return `
+      <div class="notification ${this.type ?? ''}" style="--value: ${this.duration}ms">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type ?? ''}</div>
+          <div class="notification-body">
+            ${this.text}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  // Main
+  createElement() {
+    const element = document.createElement('div');
+    element.innerHTML = this.createNotificationTemplate();
+    return element.firstElementChild;
+  }
+
+  saveLastShownNotification() {
+    if (NotificationMessage.lastShownNotification) {
+      NotificationMessage.lastShownNotification.hide();
+    }
+    NotificationMessage.lastShownNotification = this;
+  }
+
+  show(container = document.body) {
+    this.saveLastShownNotification();
+
+    if (this.duration) {
+      this.timerId = setTimeout(() => {
+        this.hide();
+      }, this.duration);
+    }
+
+    container.append(this.element);
+  }
+
+  hide() {
+    this.destroy();
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    clearTimeout(this.timerId);
+    this.remove();
+  }
 }
